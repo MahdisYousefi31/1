415a49ea7ba326c2303e427a256e2127c8f4f6ac
MahdisYousefi31 <mahdis.yousefi84@gmail.com>
2024-11-02 17:14:01 +0330

    Add files via upload

---
+++ PrimeNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeTest
+{
+    class One
+    {
+        static void Main(String[] args)
+    {
+            int n = 2;
+            int m = 40;
+            bool isPrime = true;
+
+            for (int j = n; j < m; j++)
+            {
+                for (int i = j - 1; i > 1; i--)
+                {
+                    if (j % i == 0)
+                        isPrime = false;
+                }
+                if (isPrime) Console.WriteLine(j);
+                 
+                isPrime = true;
+            }
+           Console.ReadLine();
+       }
+   }
+}
+
+
+     
